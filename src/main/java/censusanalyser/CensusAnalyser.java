1dab4528cc522978b8d2b76079bc64fd2b95fb5b
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-07 04:36:03 +0530

    ADDED:code to throw exception csv file with incorrect delimeter is given

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -29,6 +29,10 @@ public class CensusAnalyser {
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.WRONG_FILE_DELIMETER);
         }
+
     }
 }
